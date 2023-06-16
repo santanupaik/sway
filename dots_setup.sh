f98403e4782636a619f3006424f80ab616089fb9
Libereco <127647619+liberec0@users.noreply.github.com>
2023-06-16 17:55:32 +0530

    Update dots_setup.sh

--- dots_setup.sh
+++ dots_setup.sh
@@ -7,18 +7,17 @@ cp -r swappy /home/$USER/.config/
 cp -r sway /home/$USER/.config/
 cp -r swaylock /home/$USER/.config/
 cp -r waybar /home/$USER/.config/
-cp -r fonts /home/$USER/.local/share/
 sync
 
 mkdir -p -v /home/$USER/Pictures/Screenshots
+mkdir /home/$USER/.local/share/fonts
+mkdir /home/$USER/.local/share/icons
 mkdir /home/$USER/Downloads
 mkdir /home/$USER/Documents
-mkdir /home/$USER/.local/share/icons
-mkdir /home/$USER/.wallpapers
 mkdir /home/$USER/.icons
 mkdir /home/$USER/.themes
 
-cp -r wallpapers/ /home/$USER/.wallpapers/
+unzip wallpapers/wallpapers.zip -d /home/$USER/
 unzip themes/Tokyonight-Dark-B.zip -d /home/$USER/.themes/
 unzip icons/Cursor.zip -d /home/$USER/.icons/
 unzip fonts/Ubuntu.zip -d /home/$USER/.local/share/fonts/
