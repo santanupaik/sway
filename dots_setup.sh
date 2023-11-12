471e5184decef65efd7a93583e28869f2be5810e
Santanu Paik <libereco.95@proton.me>
2023-11-13 03:15:10 +0530

    Miscellaneous updates to config files.

--- dots_setup.sh
+++ dots_setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 echo
-read -p "Setup your Desktop Environment ? (y/n): " ans
-case $ans in
+read -p "Setup your Desktop Environment ? (y/n): " sans
+case $sans in
     y|Y)
     	echo
         echo "Setup is Starting..."
@@ -20,11 +21,14 @@ case $ans in
 	exit 0
     ;;
 esac
+
 echo
 printf "Creating the initial .config Directory...                                  "
 sleep 1s
+
 mkdir -p /home/$USER/.config
 printf "Done\n"
+
 printf "Copying the necessary config files to .config...                           "
 sleep 1s
 cp -r fuzzel /home/$USER/.config/
@@ -33,8 +37,10 @@ cp -r sway /home/$USER/.config/
 cp -r swaylock /home/$USER/.config/
 cp -r waybar /home/$USER/.config/
 cp -r alacritty /home/$USER/.config/
+cp -r Thunar /home/$USER/.config/
 sync
 printf "Done\n"
+
 printf "Creating the rest of the necessary Directories...                          "
 sleep 1s
 mkdir -p /home/$USER/Pictures/Screenshots
@@ -45,6 +51,7 @@ mkdir /home/$USER/Documents
 mkdir /home/$USER/.icons
 mkdir /home/$USER/.themes
 printf "Done\n"
+
 printf "Extracting rest the necessary files to their appropriate Directories...    "
 sleep 1s
 cp -r gedit/tokyonight-dark.xml /home/$USER/.local/share/gedit/styles/
@@ -54,16 +61,35 @@ bsdtar -xf icons/Cursor.zip -C /home/$USER/.icons/
 bsdtar -xf icons/papirus-icon-theme-yaru-folders.tar.xz -C /home/$USER/.icons/ Papirus
 sync
 printf "Done\n"
+
 printf "Linking the necessary Directories...                                       "
 sleep 1s
 ln -sf /home/$USER/.icons/Papirus /home/$USER/.local/share/icons/
 ln -sf /home/$USER/.themes/Tokyonight-Dark-B/gtk-4.0 /home/$USER/.config/
 printf "Done\n"
+
 printf "Appending required variables to .bash_profile...                           "
 sleep 1s
 cat bash_profile-append >> /home/$USER/.bash_profile
+cat electron-append >> /home/$USER/.config/electron-flags.conf
+cat electron-append >> /home/$USER/.config/code-flags.conf
 printf "Done\n"
+
 echo
-echo "Note: A reboot is required to complete the setup."
+read -p "Script Succeeded. Remove Cloned Repository ? (y/n): " dans
+case $dans in
+    y|Y)
+        rm -rf /home/$USER/userdots && echo "DONE\n"
+    ;;
+    n|N)
+        echo "Keeping Cloned Repo Intact."
+    ;;
+    *)
+        echo "Invalid Choice."
+    ;;
+esac
+
+echo
+echo "Note: A reboot is Recommended."
 echo "Success !!"
 echo
--- required_packages.sh
+++ required_packages.sh
@@ -22,17 +22,21 @@ case $ans in
 	exit 0
     ;;
 esac
+
 echo
 sudo pacman -Syu --noconfirm
 echo
+
 echo "Installing all the Required Packages..."
 sleep 1s
+
 echo
 sudo pacman -S --noconfirm linux linux-headers base-devel openssh linux-firmware vim git networkmanager wpa_supplicant wireless_tools netctl dialog
 sudo pacman -S --noconfirm amd-ucode sbctl efibootmgr dosfstools mtools
 sudo pacman -S --noconfirm mesa wayland sway swaylock swayidle swaybg fuzzel waybar gvfs alacritty thunar fragments firefox chromium celluloid gvfs-smb torbrowser-launcher firejail pipewire pipewire-pulse gvfs-mtp gsettings-desktop-schemas pamixer pavucontrol otf-font-awesome grim slurp swappy
 sudo pacman -S --noconfirm gedit ristretto corectrl qt5-wayland qt6-wayland wget android-tools exfatprogs zathura zathura-pdf-mupdf usbutils htop neofetch keepassxc thunar-archive-plugin thunar-volman tumbler file-roller pwgen ttf-dejavu noto-fonts-cjk noto-fonts-emoji noto-fonts code ttf-hack-nerd ttf-sourcecodepro-nerd ttf-ubuntu-font-family
 sleep 1s
+
 echo
 echo "Success !!"
 echo
