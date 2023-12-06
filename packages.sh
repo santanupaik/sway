9b565c5dac40c4666f0a2d0f8c815e7f0f83f50a
Santanu Paik <libereco.95@proton.me>
2023-12-06 16:28:02 +0530

    Cleaned up and updated.

--- packages.sh
+++ packages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo
-[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+echo "NOTE: ONLY RUN THIS SCRIPT IN ROOT, ELSE IT WILL FAIL !"
+echo
 read -p "Update the System & install all the required packages ? (y/n): " ans
 
 case $ans in
@@ -24,17 +25,22 @@ case $ans in
 esac
 
 echo
-sudo pacman -Syu --noconfirm
+pacman -Syu --noconfirm
 echo
 
 echo "Installing all the Required Packages..."
 sleep 1s
 
 echo
-sudo pacman -S --noconfirm linux linux-headers base-devel openssh linux-firmware vim git networkmanager wpa_supplicant wireless_tools netctl dialog
-sudo pacman -S --noconfirm amd-ucode sbctl efibootmgr dosfstools mtools
-sudo pacman -S --noconfirm mesa libva-mesa-driver wayland sway swaylock swayidle swaybg fuzzel waybar gvfs alacritty thunar fragments firefox chromium celluloid gvfs-smb torbrowser-launcher firejail pipewire pipewire-pulse gvfs-mtp gsettings-desktop-schemas pamixer pavucontrol otf-font-awesome grim slurp swappy
-sudo pacman -S --noconfirm gedit ristretto corectrl qt5-wayland qt6-wayland wget android-tools exfatprogs zathura zathura-pdf-mupdf usbutils htop neofetch keepassxc thunar-archive-plugin thunar-volman tumbler file-roller pwgen ttf-dejavu noto-fonts-cjk noto-fonts-emoji noto-fonts code ttf-hack-nerd ttf-sourcecodepro-nerd ttf-ubuntu-font-family
+pacman -S --noconfirm linux linux-headers base-devel openssh linux-firmware vim git networkmanager netctl dialog
+pacman -S --noconfirm amd-ucode sbctl efibootmgr dosfstools mtools
+pacman -S --noconfirm mesa libva-mesa-driver wayland sway swaylock swayidle swaybg fuzzel waybar gvfs alacritty \
+                      thunar fragments celluloid gvfs-smb torbrowser-launcher pipewire pipewire-pulse gvfs-mtp \
+                      gsettings-desktop-schemas pamixer pavucontrol otf-font-awesome grim slurp swappy
+pacman -S --noconfirm gedit ristretto corectrl qt5-wayland qt6-wayland wget android-tools exfatprogs zathura \
+                      zathura-pdf-mupdf usbutils htop neofetch keepassxc thunar-archive-plugin thunar-volman \
+                      tumbler file-roller pwgen ttf-dejavu noto-fonts-cjk noto-fonts-emoji noto-fonts code \
+                      ttf-hack-nerd ttf-sourcecodepro-nerd ttf-ubuntu-font-family
 sleep 1s
 
 echo
