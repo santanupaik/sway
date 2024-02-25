20ba07616fe04e449f160ab7df6079af6a65110e
itnerd7 <itnerd7@proton.me>
2024-02-25 17:44:37 +0530

    Removed third-party theme files

--- setup.sh
+++ setup.sh
@@ -23,14 +23,30 @@ case $sans in
 esac
 
 echo
-printf "Creating the initial .config Directory...                                  "
+printf "Creating necessary directories...                       "
 sleep 1s
 
 mkdir -p /home/$USER/.config
 mkdir -p /home/$USER/.local/share
+mkdir themes
 printf "DONE\n"
 
-printf "Copying the necessary config files to .config...                           "
+printf "Downloading Theme files... (This might take a while)    "
+wget -q https://master.dl.sourceforge.net/project/swaywm/Win-8.1-S.tar.gz -P themes
+wget -q https://master.dl.sourceforge.net/project/swaywm/Tokyonight-Dark-B.tar.gz -P themes
+wget -q https://master.dl.sourceforge.net/project/swaywm/Papirus.tar.gz -P themes
+printf "DONE\n"
+
+printf "Extracting Theme files...                               "
+tar -xzf themes/Win-8.1-S.tar.gz -C themes 2> /dev/null
+tar -xzf themes/Tokyonight-Dark-B.tar.gz -C themes 2> /dev/null 
+tar -xzf themes/Papirus.tar.gz -C themes 2> /dev/null 
+rm themes/Win-8.1-S.tar.gz
+rm themes/Tokyonight-Dark-B.tar.gz
+rm themes/Papirus.tar.gz
+printf "DONE\n"
+
+printf "Copying the necessary config files to .config...        "
 sleep 1s
 cp -r fuzzel /home/$USER/.config/
 cp -r swappy /home/$USER/.config/
@@ -44,7 +60,7 @@ cp -r themes/Tokyonight-Dark-B/"gtk-4.0" /home/$USER/.config/
 sync
 printf "DONE\n"
 
-printf "Creating rest of the necessary Directories...                              "
+printf "Creating rest of the necessary Directories...           "
 sleep 1s
 mkdir -p /home/$USER/Pictures/Screenshots
 mkdir /home/$USER/.local/share/icons
@@ -54,15 +70,15 @@ mkdir /home/$USER/Downloads
 mkdir /home/$USER/Documents
 printf "DONE\n"
 
-printf "Moving rest of the necessary files to their appropriate Directories...     "
+printf "Moving rest of the necessary files...                   "
 sleep 1s
 mv wallpapers/assets/* /home/$USER/.local/share/wallpapers/
-mv themes/* /home/$USER/.local/share/themes/
-mv icons/* /home/$USER/.local/share/icons/
+mv themes/Tokyonight-Dark-B /home/$USER/.local/share/themes/
+mv themes/* /home/$USER/.local/share/icons/
 sync
 printf "DONE\n"
 
-printf "Appending strings to their required config files...                        "
+printf "Appending strings to their required config files...     "
 sleep 1s
 < misc/bash-profile tee -a /home/$USER/.bash_profile > /dev/null
 < misc/electron-flags tee -a /home/$USER/.config/electron-flags.conf > /dev/null
@@ -85,6 +101,6 @@ case $dans in
 esac
 
 echo
-echo "Note: A reboot is Recommended."
+echo "Note: You must logout and log back in again."
 echo "Success !!"
 echo
